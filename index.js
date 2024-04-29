e069b747b5e21a784a86ef42fe6989cc51db5f6a
Haim Kastner <hello@haim-kastner.com>
2024-04-30 00:02:39 +0300

    Update swagger

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 // Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Mock API response for /length endpoint
 app.get('/length', (req, res) => {
